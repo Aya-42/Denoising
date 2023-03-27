8c5ab6af8ea6be8e1a251941afc639f8a86ed497
cap175 <66417492+cap175@users.noreply.github.com>
2023-03-27 21:53:31 +0300

    results compared to traditional filter

--- Test.py
+++ Test.py
@@ -2,6 +2,7 @@ from keras.models import load_model
 import numpy as np
 import matplotlib.pyplot as plt
 from keras.datasets import mnist
+import cv2
 
 # Load the saved model
 model = load_model('denoising_autoencoder.h5')
@@ -20,21 +21,40 @@ x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 # Use the model to denoise the images
 decoded_imgs = model.predict(x_test_noisy)
 
+# Apply median filtering to the noisy images
+x_test_noisy_filtered = np.zeros_like(x_test_noisy)
+for i in range(len(x_test_noisy)):
+    x_test_noisy_filtered[i] = cv2.medianBlur((x_test_noisy[i] * 255).astype(np.uint8), 3) / 255
+
 # Display some sample results
 n = 10  # Number of images to display
-plt.figure(figsize=(20, 4))
+plt.figure(figsize=(20, 8))
 for i in range(n):
     # Display original images
-    ax = plt.subplot(2, n, i + 1)
+    ax = plt.subplot(3, n, i + 1)
     plt.imshow(x_test_noisy[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
+    if i == 0:
+        ax.set_title('Original')
+
+    # Display noisy images
+    ax = plt.subplot(3, n, i + n + 1)
+    plt.imshow(x_test_noisy_filtered[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    if i == 0:
+        ax.set_title('Median Filter')
 
     # Display denoised images
-    ax = plt.subplot(2, n, i + n + 1)
+    ax = plt.subplot(3, n, i + 2*n + 1)
     plt.imshow(decoded_imgs[i].reshape(28, 28))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
+    if i == 0:
+        ax.set_title('Autoencoder')
+
 plt.show()
